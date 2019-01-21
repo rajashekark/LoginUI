00cf7618324060fb99474aa40d246c4778e4ade7
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-21 22:20:03 +0530

    added loginservice to communicate with springapi

---
+++ login-UI/src/app/login-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+//import { HttpClient } from '../../node_modules/@types/selenium-webdriver/http';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../node_modules/rxjs';
+import { UserDetails } from './user-details';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(  private http: HttpClient) { }
+
+url = 'http://' + environment.ipAddress + ':'+environment.port + '/user';
+
+
+public getUserDetails() {
+  return this
+  .http
+  .get<UserDetails[]>(`${this.url}/showUserDetails`)
+}
+
+}
+
