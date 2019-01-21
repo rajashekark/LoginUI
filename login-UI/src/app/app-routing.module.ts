805d37bae0b4202bce2464daa231961af40b9147
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-21 10:17:02 +0530

    added routing to register component

--- login-UI/src/app/app-routing.module.ts
+++ login-UI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'userRegister', component:UserRegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
