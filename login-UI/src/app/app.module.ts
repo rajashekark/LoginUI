4315d68ed7425a3cf3712916f68ad7977d7f99dc
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-21 10:18:29 +0530

    added components and changed home page

--- login-UI/src/app/app.module.ts
+++ login-UI/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { LoginServiceModule } from './login-service.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
